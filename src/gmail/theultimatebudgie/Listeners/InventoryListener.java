745b5543bea88277ecfee1c5d1d35e4139ae3373
Timtower <timdebrouwer@live.nl>
2014-01-02 23:29:06 +0100

    Update InventoryListener.java
    Cleanup

--- src/gmail/theultimatebudgie/Listeners/InventoryListener.java
+++ src/gmail/theultimatebudgie/Listeners/InventoryListener.java
@@ -1,17 +1,10 @@
 package gmail.theultimatebudgie.Listeners;
 
-import gmail.theultimatebudgie.ZombieSurvival.Utilities;
 import gmail.theultimatebudgie.ZombieSurvival.ZombieCore;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.inventory.InventoryType;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 
 public class InventoryListener implements Listener {
 	ZombieCore plugin;
